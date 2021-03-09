ae6d6c345a3d83445507b0570cf1c0af9f02f60a
alswjd1125 <77230028+yuul2@users.noreply.github.com>
2021-03-09 13:16:16 +0900

    20210309 am1 recently

--- js/main.js
+++ js/main.js
@@ -8,5 +8,66 @@ $(function(){
             $('.nav-c').css('color','#ffffff')
         }
     });
+});
+// navigator jquery
 
+$(function(){
+    $('.col-pk').click(function(){
+        $('.sec-03-top-none').css('display','block');
+        $('.sec-03-top-none-sum').css('display','none');
+        $('.sec-03-top-none-fall').css('display','none');
+        $('.sec-03-top-none-win').css('display','none');
+       
+    });
+    $('#col-pk').click(function(){
+        $('.sec03-img-spring').css('display','block');
+        $('.sec03-img-summer').css('display','none');
+        $('.sec03-img-fall').css('display','none');
+        $('.sec03-img-winter').css('display','none');
+    });
+});
+
+$(function(){
+    $('.col-gn').click(function(){
+        $('.sec-03-top-none-sum').css('display','block');
+        $('.sec-03-top-none').css('display','none');
+        $('.sec-03-top-none-fall').css('display','none');
+        $('.sec-03-top-none-win').css('display','none');
+    });
+    $('#col-gn').click(function(){
+        $('.sec03-img-summer').css('display','block');
+        $('.sec03-img-spring').css('display','none');
+        $('.sec03-img-fall').css('display','none');
+        $('.sec03-img-winter').css('display','none');
+    });
+});
+
+$(function(){
+    $('.col-br').click(function(){
+        $('.sec-03-top-none-fall').css('display','block');
+        $('.sec-03-top-none').css('display','none');
+        $('.sec-03-top-none-sum').css('display','none');
+        $('.sec-03-top-none-win').css('display','none');
+    });
+    $('#col-br').click(function(){
+        $('.sec03-img-fall').css('display','block');
+        $('.sec03-img-spring').css('display','none');
+        $('.sec03-img-summer').css('display','none');
+        $('.sec03-img-winter').css('display','none');
+    });
+});
+
+$(function(){
+    $('.col-gr').click(function(){
+        $('.sec-03-top-none-win').css('display','block');
+        $('.sec-03-top-none').css('display','none');
+        $('.sec-03-top-none-sum').css('display','none');
+        $('.sec-03-top-none-fall').css('display','none');
+    });
+    $('#col-gr').click(function(){
+        $('.sec03-img-winter').css('display','block');
+        $('.sec03-img-spring').css('display','none');
+        $('.sec03-img-summer').css('display','none');
+        $('.sec03-img-fall').css('display','none');
+    });
 });
